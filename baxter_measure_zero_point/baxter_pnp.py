c3f8f19e4fb1f2a4acd84c60a0dcbdd64a947870
Hubert51 <gengr@rpi.edu>
2019-06-14 16:10:13 -0400

    upload the scripts for null changes

--- baxter_measure_zero_point/baxter_pnp.py
+++ baxter_measure_zero_point/baxter_pnp.py
@@ -49,9 +49,9 @@ p = PlanningSceneInterface("base")
 leftgripper = baxter_interface.Gripper('left')
 rightgripper = baxter_interface.Gripper('right')
 # leftgripper.calibrate()
-# rightgripper.calibrate()
+rightgripper.calibrate()
 # leftgripper.open()
-# rightgripper.open()
+rightgripper.open()
 
 scene = moveit_commander.PlanningSceneInterface()
 robot = moveit_commander.RobotCommander()
@@ -66,7 +66,7 @@ rospy.sleep(2)
 # scene.add_box("table", box_pose, (0.5, 1.5, 0.6))
 
 # name size and position
-p.addBox("first part", 0.8, 0.43, 0.04, 1.23, 0, 0.36)
+p.addBox("first part", 0.8, 0.43, 0.02, 1.23, 0, 0.34)
 p.addBox("under part", 0.8, 0.43, 0.04, 1.23, 0, 0.0)
 
 p.addBox("second part", 0.8, 0.01, 1.6, 1.23, -0.22, 0)
@@ -352,40 +352,68 @@ def move_arm():
     # explore the API
     # utils.explore_pose_goal(both_arms)
 
-    robotArm = Baxter_impl()
-    print robotArm.joint_positions()
+    #######
+    # the functionality in jointcontroller_host
+    # robotArm = Baxter_impl() 
+    # print robotArm.joint_positions()
+    ########
+
     # rospy.Subscriber("chatter", String, callback)
     # joint_state =rospy.wait_for_message("/robot/joint_states", JointState) 
-    # utils.get_current_joint_state()
-    #print(joint_state)
+    # micro_front_door3 = {'left_w0': -1.4733885467639398, 'left_w1': -1.570029336400721, 'left_w2': 1.3433836749906511, 'right_s0': 0.45405831321408247, 'right_s1': -0.7765777738669907, 'right_w0': 0.5767767762449155, 'right_w1': -1.2540292940963258, 'right_w2': 0.3466796580621035, 'left_e0': -1.7161410064468066, 'left_e1': 2.5322187856018465, 'left_s0': 1.5523885573400387, 'left_s1': 0.033364082136507746, 'right_e0': 1.2505778373235836, 'right_e1': 1.8952332634324287}
 
+    # both_arms.set_joint_value_target(micro_front_door3)
+    # both_arms.plan()
+    # both_arms.go(wait=True)
+    # raw_input("plz")
+    utils.get_current_joint_state()
+    #print(joint_state)
+    
+    moveit_commander.os._exit(0)
+
+
+    # one set of workable path
     # init_pos = {'left_w0': -2.223505152039907, 'left_w1': 0.01802427425765361, 'left_w2': -1.190369091399081, 'right_s0': 0.4663301595171658, 'right_s1': 1.0365875174135684, 'right_w0': 2.1828546611609436, 'right_w1': 1.6616846884768743, 'right_w2': -2.28102943158561, 'left_e0': 0.027611654181937447, 'left_e1': 1.5627429276582652, 'left_s0': 0.858262250821889, 'left_s1': -0.04486893804564835, 'right_e0': 1.6110633224766557, 'right_e1': 2.249582825433959}
     # fridge_pos = {'left_w0': -2.222354666448993, 'left_w1': 0.01802427425765361, 'left_w2': -1.190369091399081, 'right_s0': 0.9664078963678107, 'right_s1': 0.40382044241083515, 'right_w0': 2.277577974812868, 'right_w1': 0.6143593055481082, 'right_w2': -1.3395487230209375, 'left_e0': 0.027228158984966094, 'left_e1': 1.5600584612794657, 'left_s0': 0.858262250821889, 'left_s1': -0.04410194765170564, 'right_e0': 2.0470973614330847, 'right_e1': 0.3834951969713534}
     # microwave = {'left_w0': -2.222354666448993, 'left_w1': 0.01802427425765361, 'left_w2': -1.1896021010051383, 'right_s0': 0.7033301912454623, 'right_s1': 0.49125734732030374, 'right_w0': 2.790694548360539, 'right_w1': 1.1635244276110863, 'right_w2': -2.3834226491769614, 'left_e0': 0.026461168591023387, 'left_e1': 1.5627429276582652, 'left_s0': 0.8578787556249177, 'left_s1': -0.04601942363656241, 'right_e0': 2.3431556534949696, 'right_e1': 1.504068162521648}
+    
+    # matlab workable path
+    init_pos = {'left_w0': -1.472238061173026, 'left_w1': -1.5707963267946636, 'left_w2': 1.3430001797936797, 'right_s0': 0.4973932704718454, 'right_s1': 0.019941750242510378, 'right_w0': 2.9575149590430776, 'right_w1': 1.9209274416295095, 'right_w2': -2.656087734223594, 'left_e0': -1.716524501643778, 'left_e1': 2.5322187856018465, 'left_s0': 1.5527720525370101, 'left_s1': 0.03413107253045045, 'right_e0': 1.0074418824437454, 'right_e1': 2.1360682471304386}
+    fridge_pos = {'left_w0': -1.472238061173026, 'left_w1': -1.5707963267946636, 'left_w2': 1.3430001797936797, 'right_s0': 0.6250971710633061, 'right_s1': -0.0395000052880494, 'right_w0': 3.046869339937403, 'right_w1': 1.151252581308003, 'right_w2': -2.6488013254811382, 'left_e0': -1.7161410064468066, 'left_e1': 2.5318352904048753, 'left_s0': 1.5527720525370101, 'left_s1': 0.0337475773334791, 'right_e0': 1.0193302335498575, 'right_e1': 1.2870098810358621}
+    microwave = {'left_w0': -1.4733885467639398, 'left_w1': -1.5692623460067783, 'left_w2': 1.3433836749906511, 'right_s0': 0.7056311624272903, 'right_s1': -0.5867476513661708, 'right_w0': 0.6040049352298816, 'right_w1': -0.6768690226544388, 'right_w2': 0.1721893434401377, 'left_e0': -1.7157575112498353, 'left_e1': 2.531451795207904, 'left_s0': 1.551621566946096, 'left_s1': 0.033364082136507746, 'right_e0': 0.8950777897311389, 'right_e1': 1.112136071216925}
+    micro_front_door = {'left_w0': -1.4726215563699971, 'left_w1': -1.571179821991635, 'left_w2': 1.3430001797936797, 'right_s0': 0.400368985638093, 'right_s1': -0.06557767868210143, 'right_w0': 3.0591411862404865, 'right_w1': 1.6570827461132183, 'right_w2': -3.0579907006495723, 'left_e0': -1.716524501643778, 'left_e1': 2.5322187856018465, 'left_s0': 1.5531555477339813, 'left_s1': 0.034514567727421806, 'right_e0': 1.507136124097419, 'right_e1': 2.0563012461603973}
+    micro_front_door2 = {'left_w0': -1.472238061173026, 'left_w1': -1.5707963267946636, 'left_w2': 1.3437671701876224, 'right_s0': 0.4356505437594575, 'right_s1': 0.05253884198507542, 'right_w0': 3.0595246814374577, 'right_w1': 1.5251603983550726, 'right_w2': -0.17180584824316633, 'left_e0': -1.7157575112498353, 'left_e1': 2.5322187856018465, 'left_s0': 1.5520050621430674, 'left_s1': 0.03413107253045045, 'right_e0': 1.7307138239317181, 'right_e1': 1.880276950750546}
+    
+    micro_front_door3 = {'left_w0': -1.4733885467639398, 'left_w1': -1.570029336400721, 'left_w2': 1.3433836749906511, 'right_s0': 0.45405831321408247, 'right_s1': -0.7765777738669907, 'right_w0': 0.5767767762449155, 'right_w1': -1.2540292940963258, 'right_w2': 0.3466796580621035, 'left_e0': -1.7161410064468066, 'left_e1': 2.5322187856018465, 'left_s0': 1.5523885573400387, 'left_s1': 0.033364082136507746, 'right_e0': 1.2505778373235836, 'right_e1': 1.8952332634324287}
 
-    # print "-----> move to the initial position"
+    print "-----> move to the initial position"
 
-    # both_arms.set_joint_value_target(init_pos)
-    # both_arms.plan()
-    # both_arms.go(wait=True)
+    both_arms.set_joint_value_target(init_pos)
+    both_arms.plan()
+    both_arms.go(wait=True)
 
-    # print "-----> move inside the fridge position"
+    print "-----> move inside the fridge position"
 
-    # both_arms.set_joint_value_target(fridge_pos)
-    # both_arms.plan()
-    # both_arms.go(wait=True)
+    both_arms.set_joint_value_target(fridge_pos)
+    both_arms.plan()
+    both_arms.go(wait=True)
 
-    # print "-----> move to the initial position"
+    rightgripper.close()
+    rospy.sleep(2)
 
-    # both_arms.set_joint_value_target(init_pos)
-    # both_arms.plan()
-    # both_arms.go(wait=True)
+    both_arms.set_joint_value_target(init_pos)
+    both_arms.plan()
+    both_arms.go(wait=True)
 
-    # print "-----> move inside the microwave position"
+    both_arms.set_joint_value_target(micro_front_door3)
+    both_arms.plan()
+    both_arms.go(wait=True)
 
-    # both_arms.set_joint_value_target(microwave)
-    # both_arms.plan()
-    # both_arms.go(wait=True)
+    print "-----> move inside the microwave position"
+
+    both_arms.set_joint_value_target(microwave)
+    both_arms.plan()
+    both_arms.go(wait=True)
 
     moveit_commander.os._exit(0)
 
